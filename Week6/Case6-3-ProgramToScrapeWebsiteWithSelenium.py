d88cd5b652bed174eb61a9180c116d2850204ec9
mominamughal07 <mominamughal060704@gmail.com>
2025-09-20 22:13:05 -0700

    week 6

--- Week6/Case6-3-ProgramToScrapeWebsiteWithSelenium.py
+++ Week6/Case6-3-ProgramToScrapeWebsiteWithSelenium.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 df = pd.DataFrame(columns=['Player','Salary','Year']) # creates master dataframe
 
-cService = webdriver.ChromeService(executable_path='C:\\Users\\HP\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe') # '/Users/bpfalz/Downloads/chromedriver' for my macbook
+cService = webdriver.ChromeService(executable_path='C:\\Users\\Lenovo\\Downloads\\chromedriver-win64.exe') # '/Users/bpfalz/Downloads/chromedriver' for my macbook
 driver = webdriver.Chrome(service=cService)
 
 for yr in range(1990,2023):
--- Week6/Case6-3B-ProgramToScrapeWebsiteWithSelenium.py
+++ Week6/Case6-3B-ProgramToScrapeWebsiteWithSelenium.py
@@ -5,13 +5,13 @@ import csv
 
 url = "http://www.values.com/inspirational-quotes"
 
-cService = webdriver.ChromeService(executable_path='C:\\Users\\HP\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe') # '/Users/bpfalz/Downloads/chromedriver' for my macbook
+cService = webdriver.ChromeService(executable_path='C:/Users/Lenovo/Downloads/chromedriver.exe') # '/Users/bpfalz/Downloads/chromedriver' for my macbook
 driver = webdriver.Chrome(service=cService)
 
 driver.get(url)
 
 qouestList=[]
-qoutesDiv = driver.find_elements(By.XPATH, "//div[contains(@class, 'col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top')]")
+qoutesDiv = driver.find_elements(By.XPATH, "//div[contains(@class, 'text-center mb-8')]")
 for p in range(len(qoutesDiv)):
     quote = {}
     quote['theme'] = qoutesDiv[p].text
@@ -22,7 +22,7 @@ for p in range(len(qoutesDiv)):
     quote['url'] = innera.get_attribute('href')
     qouestList.append(quote)
 
-filename = 'Week4/inspirational_quotes.csv'
+filename = 'Week6/inspirational_quotes.csv'
 with open(filename, 'w', newline='') as f:
     w = csv.DictWriter(f,['theme','url','img','lines','author'])
     w.writeheader()
